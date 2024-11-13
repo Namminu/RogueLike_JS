45062fc5539c6b84cad6a532e20701949a0a170c
20191143namminwoo <mvpalsdn771@gmail.com>
2024-11-13 17:53:26 +0900

    스테이지 넘김 현상 해결
    json 파일의 데이터 문제였음
    해결 완료

--- RogueLike/Battle.js
+++ RogueLike/Battle.js
@@ -5,13 +5,10 @@ import readlineSync from 'readline-sync';
 
 export const battle = async (stage, player) => {
     let logs = [];
-
     const monster = spawnMonster(stage);
 
-    while (player.CurHP > 0 || monster.HP > 0) 
-    {
-        if(player.CurHP <= 0) 
-        {
+    while (player.CurHP > 0 || monster.HP > 0) {
+        if (player.CurHP <= 0) {
             console.log(chalk.red('플레이어가 사망했습니다.. 게임을 종료합니다.'));
             process.exit(0);
         }
@@ -19,7 +16,7 @@ export const battle = async (stage, player) => {
 
         console.clear();
         console.log(monster.TYPE === 'Normal' ? chalk.green(`[ 일반 스테이지 ${stage}]`) :
-                            chalk.red(`[ 보스 스테이지 ${stage}]`));
+            chalk.red(`[ 보스 스테이지 ${stage}]`));
         displayStatus(stage, player, monster);
 
         logs.forEach((log) => console.log(log));
@@ -29,31 +26,29 @@ export const battle = async (stage, player) => {
                 `\n1. 공격   2. 회피(35%)`,
             ),
         );
-        battleInput(logs, player, monster);        
+        battleInput(logs, player, monster);
     }
 };
 
-function battleInput(logs, player, monster)
-{
+function battleInput(logs, player, monster) {
     let pDamage, mDamage;
     const choice = readlineSync.question('Enter : ');
-    switch(choice)
+    switch (choice) 
     {
-        case '1' :
+        case '1':
             //플레이어 공격 처리 
             pDamage = player.Attack();
             logs.push(chalk.green(`공격 : ${pDamage.message}`));
             logs.push(monster.Mon_Hitted(pDamage.damage));
 
             //이후 몬스터 공격 진행
-            setTimeout(()=> {
-                mDamage = monster.Mon_Attack();
-                logs.push(mDamage.message);
-                player.Hitted(mDamage.damage);
-            }, 1000)
+            // setTimeout(()=> {}, 1000)
+            mDamage = monster.Mon_Attack();
+            logs.push(mDamage.message);
+            player.Hitted(mDamage.damage);
             break;
 
-        case '2' :
+        case '2':
             mDamage = monster.Mon_Attack();
             logs.push(chalk.green(`회피 : ${player.Dodge(mDamage.damage)}`));
             break;
